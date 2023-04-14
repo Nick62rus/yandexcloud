36126fb2a02c7d0fdedefa1cf91db8936a3bd04f
Nick62 <zubkovmain@gmail.com>
2023-04-14 15:34:39 +0300

    new file with vm

---
+++ make_vm_from_ya_cli_with_parameters.ps1
@@ -0,0 +1,9 @@
+﻿# Создание VM через CLI
+yc compute instance create `
+  --name demo-1 `
+  --hostname demo-1 `
+  --metadata-from-file user-data=startup.sh `
+  --create-boot-disk image-folder-id=standart-images,image-family=ubuntu-2004-lts `
+  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 `
+  --zone ru-central1-a `
+  --ssh-key %HOMEPATH%\.ssh\id_ed25519.pub
